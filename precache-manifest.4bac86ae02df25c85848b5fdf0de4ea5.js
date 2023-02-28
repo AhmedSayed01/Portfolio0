aaeea49b991e497f7e9abd9412b7a659d7740f3a
AhmedSayed01 <ahmedsayed2017@hotmail.com>
2023-02-28 15:30:40 +0200

    Updates

---
+++ precache-manifest.4bac86ae02df25c85848b5fdf0de4ea5.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "50e138cd04465ca68c40",
+    "url": "/Portfolio0/static/css/main.068b9182.chunk.css"
+  },
+  {
+    "revision": "50e138cd04465ca68c40",
+    "url": "/Portfolio0/static/js/main.50e138cd.chunk.js"
+  },
+  {
+    "revision": "49f4a745e9a43a153cbd",
+    "url": "/Portfolio0/static/js/1.49f4a745.chunk.js"
+  },
+  {
+    "revision": "578dfa274c97f30440c2",
+    "url": "/Portfolio0/static/css/2.9a4e4aae.chunk.css"
+  },
+  {
+    "revision": "578dfa274c97f30440c2",
+    "url": "/Portfolio0/static/js/2.578dfa27.chunk.js"
+  },
+  {
+    "revision": "9793b27d45938e6f99d7",
+    "url": "/Portfolio0/static/js/runtime~main.9793b27d.js"
+  },
+  {
+    "revision": "a200dcf5e7f1a770b4c12224ed37d769",
+    "url": "/Portfolio0/static/media/website1image1.a200dcf5.png"
+  },
+  {
+    "revision": "2b28edca16b7a4b3032c9491e7cff2e6",
+    "url": "/Portfolio0/static/media/website2image1.2b28edca.png"
+  },
+  {
+    "revision": "fd8b61f55433b118a6e76b1949025205",
+    "url": "/Portfolio0/static/media/linkedin.fd8b61f5.svg"
+  },
+  {
+    "revision": "b4de458b1ab9a93e567295087360b846",
+    "url": "/Portfolio0/static/media/github400.b4de458b.svg"
+  },
+  {
+    "revision": "71f350c3017d0883d67de1f1fd77e90e",
+    "url": "/Portfolio0/static/media/instagram.71f350c3.svg"
+  },
+  {
+    "revision": "7944a0e47e81736bb810c40c46dc43ad",
+    "url": "/Portfolio0/static/media/website3.7944a0e4.png"
+  },
+  {
+    "revision": "76b5a3250b231f99abe3bf34c0d2d21b",
+    "url": "/Portfolio0/static/media/website1image2.76b5a325.png"
+  },
+  {
+    "revision": "6ac23a7505b0964e17d12e48c45da70e",
+    "url": "/Portfolio0/static/media/website1image3.6ac23a75.png"
+  },
+  {
+    "revision": "c0f966dfe7d4903b8f46faa23a457c3d",
+    "url": "/Portfolio0/static/media/website2image2.c0f966df.png"
+  },
+  {
+    "revision": "d5f9c0dbf34842f956556465db8ad971",
+    "url": "/Portfolio0/static/media/website2image3.d5f9c0db.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Portfolio0/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Portfolio0/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Portfolio0/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "b59974e4940d874975010d1d3d1b7618",
+    "url": "/Portfolio0/static/media/download.b59974e4.svg"
+  },
+  {
+    "revision": "67794f238f50cbe9ab69fba35fc9231c",
+    "url": "/Portfolio0/static/media/gaming.67794f23.png"
+  },
+  {
+    "revision": "88cf9b6d4afc6deb925a0633810b23c6",
+    "url": "/Portfolio0/static/media/movies.88cf9b6d.png"
+  },
+  {
+    "revision": "2214eb908076f7967810a59e584b388f",
+    "url": "/Portfolio0/static/media/music.2214eb90.png"
+  },
+  {
+    "revision": "c148af9cce1e93cce8e9ffd0d2d5f7d4",
+    "url": "/Portfolio0/static/media/chakra.c148af9c.png"
+  },
+  {
+    "revision": "eab4f4e4d8a38a96edb2242fd0487b7f",
+    "url": "/Portfolio0/static/media/sports1.eab4f4e4.png"
+  },
+  {
+    "revision": "8cd6d3d81ddd8e41e13cfda3ecb01a77",
+    "url": "/Portfolio0/static/media/html.8cd6d3d8.png"
+  },
+  {
+    "revision": "5e7e28ebe25c3d4bd6cfb0d37db05850",
+    "url": "/Portfolio0/static/media/css.5e7e28eb.png"
+  },
+  {
+    "revision": "03d471c6a43c17a3496228b5b43512e6",
+    "url": "/Portfolio0/static/media/javascript.03d471c6.png"
+  },
+  {
+    "revision": "22808bae77c66bd1b33e298ea2f4320e",
+    "url": "/Portfolio0/static/media/React.22808bae.png"
+  },
+  {
+    "revision": "c13f310ebebc30b054e84a897596d4d2",
+    "url": "/Portfolio0/static/media/download.c13f310e.png"
+  },
+  {
+    "revision": "699ed04bfd5f189fd9c7706d87ca5fb6",
+    "url": "/Portfolio0/static/media/logo.699ed04b.png"
+  },
+  {
+    "revision": "bab81d36a23733d06af4e6d2aaab0161",
+    "url": "/Portfolio0/static/media/180226_A_03.bab81d36.mp4"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/Portfolio0/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/Portfolio0/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/Portfolio0/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "e167b72117fc768dd78fe7193929b462",
+    "url": "/Portfolio0/static/media/myPic.e167b721.png"
+  },
+  {
+    "revision": "4b4a0004ea2435c338ffb521621345cb",
+    "url": "/Portfolio0/static/media/website1image4.4b4a0004.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/Portfolio0/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/Portfolio0/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/Portfolio0/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/Portfolio0/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/Portfolio0/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "0a4b751d07e1454075bdb30bf3b9b0bb",
+    "url": "/Portfolio0/index.html"
+  }
+];
